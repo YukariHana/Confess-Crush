9e98ebd114950349aa092abc9991794c9702460c
YukariHana <99553082+YukariHana@users.noreply.github.com>
2022-02-12 18:25:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    introTitle: 'Anh',
+    introDesc: `Cảm ơn anh đã đến bên cạnh em để em biết như thế nào là lấy hết dũng cảm để yêu một người. 
+    Anh khiến em lần đầu tiên cảm nhận được những quan tâm, lo lắng từ một người khác mà không phải là từ ba mẹ em.
+    Cảm ơn vì tất cả những điều ngọt ngào mà anh dành cho em.`,
+    btnIntro: '^^Yêu Anh^^',
+    title: 'Valentine vui vẻ nhé 🥰',
+    desc: 'Yêu em khum ? ',
+    btnYes: 'Yêu <33',
+    btnNo: 'Yêu thì yêu, không yêu thì yêu',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Yêu thì yêu thôi làm gì có lý do <33333333',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Rảnh thì gọi cho em nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/zukahai/Confess-Gui-Duy-Nguyen' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
